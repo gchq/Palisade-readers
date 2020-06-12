99b0847b16f12b5c9df5541b4491f747bde1cd3a
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-06-12 09:49:42 +0100

    Pal 665 moved and updated Log message

--- hadoop-reader/src/main/java/uk/gov/gchq/palisade/reader/HadoopDataReader.java
+++ hadoop-reader/src/main/java/uk/gov/gchq/palisade/reader/HadoopDataReader.java
@@ -100,8 +100,8 @@ public class HadoopDataReader extends SerialisedDataReader {
             //1st attempt: process this as a URI
             inputStream = fs.open(new Path(new URI(resource.getId())));
         } catch (URISyntaxException e) {
+            LOGGER.error("Issue encountered while reading resource {} as a URI: {}", resource.getId(), e.getMessage());
             //2nd attempt: process as a string
-            LOGGER.warn("Issue encountered while reading resource {} as a URI: {}", resource.getId(), e.getMessage());
             try {
                 inputStream = fs.open(new Path(resource.getId()));
             } catch (final IOException ex) {
