a5b80b86526a7dce802aba477ab1c55f02eba399
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-06-15 10:31:09 +0100

    Pal 665 removed unused imports

--- readers-common/src/main/java/uk/gov/gchq/palisade/reader/common/DataFlavour.java
+++ readers-common/src/main/java/uk/gov/gchq/palisade/reader/common/DataFlavour.java
@@ -30,7 +30,6 @@ import uk.gov.gchq.palisade.Generated;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.StringJoiner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.util.Objects.requireNonNull;
