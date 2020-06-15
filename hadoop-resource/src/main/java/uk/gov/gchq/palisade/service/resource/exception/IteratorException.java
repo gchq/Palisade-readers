47517fe9606b3bbceeda472a0954f8c0da32975e
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-06-15 13:12:48 +0100

    Pal 665 updated exception JavaDoc

--- hadoop-resource/src/main/java/uk/gov/gchq/palisade/service/resource/exception/IteratorException.java
+++ hadoop-resource/src/main/java/uk/gov/gchq/palisade/service/resource/exception/IteratorException.java
@@ -43,7 +43,7 @@ public class IteratorException extends RuntimeException {
      * Constructs a new {@link IteratorException} with the specified detail message and cause.
      *
      * @param message   a {@link String} value detailing the error
-     * @param cause     a {@link Throwable} that caused the iss
+     * @param cause     a {@link Throwable} that caused the issue
      */
     public IteratorException(final String message, final Throwable cause) {
         super(message, cause);
