760821e9fc250c8c7a78473c2c93d08bcfb4f429
dev930018 <56113485+dev930018@users.noreply.github.com>
2021-04-22 16:58:01 +0100

    PAL-1021 fixed imports and a couple of basic methods

--- s3-resource/src/main/java/uk/gov/gchq/palisade/service/data/s3/S3ResourceService.java
+++ s3-resource/src/main/java/uk/gov/gchq/palisade/service/data/s3/S3ResourceService.java
@@ -16,13 +16,14 @@
 
 package uk.gov.gchq.palisade.service.data.s3;
 
+import akka.NotUsed;
 import akka.japi.Pair;
-import akka.stream.Attributes;
 import akka.stream.Materializer;
-import akka.stream.alpakka.s3.BucketAccess.AccessDenied$;
-import akka.stream.alpakka.s3.BucketAccess.AccessGranted$;
+import akka.stream.alpakka.s3.BucketAccess;
 import akka.stream.alpakka.s3.ListBucketResultContents;
-import akka.stream.alpakka.s3.impl.S3Stream;
+import akka.stream.alpakka.s3.ObjectMetadata;
+import akka.stream.alpakka.s3.javadsl.S3;
+import akka.stream.javadsl.Source;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,8 +35,6 @@ import uk.gov.gchq.palisade.service.resource.service.ResourceService;
 import java.util.Iterator;
 import java.util.Optional;
 
-import static akka.stream.alpakka.s3.javadsl.S3.getObjectMetadata;
-import static akka.stream.alpakka.s3.javadsl.S3.listBucket;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -50,18 +49,15 @@ public class S3ResourceService implements ResourceService {
     public static final String ERROR_GET_BY_FORMAT = "getResourcesBySerialisedFormat is not supported by the Resource Service";
     public static final String ERROR_GET_BY_TYPE = "getResourcesByType is not supported by the Resource Service";
     public static final String NOT_EXISTS = "The Bucket, %s , does not exist or you do not have access to it.";
-    private final Materializer materialiser;
+    private static final int PARALLELISM = 1;
 
-    /**
-     * Get the bucket name from the application.yaml
-     */
-    @Value("${s3-resource-service.bucket-name}")
-    private String bucketName;
+    private final String bucketName;
+    private final Materializer materializer;
 
-    public S3ResourceService(final Materializer materialiser) {
-        this.materialiser = materialiser;
+    public S3ResourceService(final String bucketName, final Materializer materializer) {
+        this.bucketName = bucketName;
+        this.materializer = materializer;
     }
-
 
     @Override
     public Iterator<LeafResource> getResourcesByResource(final Resource resource) {
@@ -72,14 +68,8 @@ public class S3ResourceService implements ResourceService {
 
     @Override
     public Iterator<LeafResource> getResourcesById(final String resourceId) {
-        if (checkIfBucketExists(bucketName, materialiser, null) == Boolean.TRUE) {
-
-            listBucket(bucketName, Optional.of(resourceId))
-                    .flatMap((ListBucketResultContents s3Resource) -> getObjectMetadata(bucketName, s3Resource.getKey(), null, null)
-                            .map(maybeMeta -> Pair.create(s3Resource, maybeMeta.orElseThrow(new RuntimeException("our bad")))))
-                    .map((resource, meta) -> ...)
-        }
-        return null;
+        return listBucketWithMetadata(resourceId)
+            .map(objectMetaPair -> ...);
     }
 
     @Override
@@ -111,25 +101,14 @@ public class S3ResourceService implements ResourceService {
         return false;
     }
 
-    /**
-     * The user has access to the existing bucket, then it will return AccessGranted
-     * The user doesn’t have access but the bucket exists so AccessDenied will be returned
-     * The bucket doesn’t exist, the method will return NotExists
-     *
-     * @param bucket the String bucket the user wants access too
-     * @param mat    the materaliser
-     * @param attr   any additional attributes
-     * @return a Boolean value if the user has access to the bucket or not
-     */
-    public Boolean checkIfBucketExists(final String bucket, final Materializer mat, final Attributes attr) {
-        var bucketAccessFuture = S3Stream.checkIfBucketExists(bucket, null, mat, attr);
+    private Source<Boolean, NotUsed> bucketExists() {
+        return S3.checkIfBucketExistsSource(bucketName)
+            .map(bucketAccess -> bucketAccess.equals(BucketAccess.accessGranted()));
+    }
 
-        if (bucketAccessFuture.isCompleted() && bucketAccessFuture instanceof AccessGranted$) {
-            return Boolean.TRUE;
-        } else if (bucketAccessFuture.isCompleted() && bucketAccessFuture instanceof AccessDenied$) {
-            return Boolean.FALSE;
-        } else {
-            throw new UnsupportedOperationException(String.format(NOT_EXISTS, bucket));
-        }
+    private Source<Pair<ListBucketResultContents, ObjectMetadata>, NotUsed> listBucketWithMetadata(final String resourceId) {
+        return S3.listBucket(bucketName, Optional.of(resourceId))
+            .flatMapMerge(PARALLELISM, s3Object -> S3.getObjectMetadata(bucketName, s3Object.getKey())
+                .map(s3Meta -> Pair.create(s3Object, s3Meta.orElseThrow(() -> new RuntimeException(String.format("Lost object '%s' while listing bucket", s3Object.getKey()))))));
     }
 }
