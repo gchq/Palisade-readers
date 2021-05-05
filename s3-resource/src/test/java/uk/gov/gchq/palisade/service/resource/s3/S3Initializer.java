fb7f3b0d3648cf52f30a5082326cd7c3e286041b
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2021-05-05 16:27:15 +0100

    PAL-1021 work on pulling test containers from yaml config

--- s3-resource/src/test/java/uk/gov/gchq/palisade/service/resource/s3/S3Initializer.java
+++ s3-resource/src/test/java/uk/gov/gchq/palisade/service/resource/s3/S3Initializer.java
@@ -16,48 +16,57 @@
 
 package uk.gov.gchq.palisade.service.resource.s3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.lang.NonNull;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 
 public class S3Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(S3Initializer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3Initializer.class);
 
-//    @Value("${testcontainers.localstack.image}")
-//    private String fullImageName;
-//    @Value("${testcontainers.localstack.default.image}")
-//    private String defaultImageName;
+    @Value("${testcontainers.localstack.image}")
+    private String fullImageName;
+    @Value("${testcontainers.localstack.default.image}")
+    private String defaultImageName;
 
-//    static LocalStackContainer lcs;
+    static LocalStackContainer lcs;
 
     @Override
     public void initialize(@NonNull final ConfigurableApplicationContext context) {
 
-//        DockerImageName localstackImageName;
-//        try {
-//            localstackImageName = DockerImageName.parse(fullImageName)
-//                    .asCompatibleSubstituteFor(defaultImageName);
-//            localstackImageName.assertValid();
-//        } catch (IllegalArgumentException ex) {
-//            LOGGER.warn("Image name {} was invalid, falling back to default name {}", fullImageName, defaultImageName, ex);
-//            localstackImageName = DockerImageName.parse(defaultImageName);
-//        }
-//
-//        final GenericContainer<?> localStackContainer = new LocalStackContainer(localstackImageName)
-//                .withServices(S3)
-//                .withReuse(true);
-//
-//        context.getEnvironment().setActiveProfiles("s3");
-//        // Start container
-//        localStackContainer.start();
-//
-//        lcs = (LocalStackContainer) localStackContainer;
-//        var accessKey = "aws.accessKeyId=" + lcs.getDefaultCredentialsProvider().getCredentials().getAWSAccessKeyId();
-//        var secretKey = "aws.secretKey=" + lcs.getDefaultCredentialsProvider().getCredentials().getAWSSecretKey();
-//        var endpoint = "aws.endpoint=" + lcs.getEndpointConfiguration(S3).getServiceEndpoint();
-//        var region = "aws.region=" + lcs.getEndpointConfiguration(S3).getSigningRegion();
-//
-//        LOGGER.info("Starting LocalStack S3 with accessKey: {}, secretKey: {}, endpoint: {}, and region: {}", accessKey, secretKey, endpoint, region);
-//        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(context, accessKey, secretKey, endpoint, region);
+        DockerImageName localstackImageName;
+        try {
+            localstackImageName = DockerImageName.parse(fullImageName)
+                    .asCompatibleSubstituteFor(defaultImageName);
+            localstackImageName.assertValid();
+        } catch (IllegalArgumentException ex) {
+            LOGGER.warn("Image name {} was invalid, falling back to default name {}", fullImageName, defaultImageName, ex);
+            localstackImageName = DockerImageName.parse(defaultImageName);
+        }
+
+        final GenericContainer<?> localStackContainer = new LocalStackContainer(localstackImageName)
+                .withServices(S3)
+                .withReuse(true);
+
+        context.getEnvironment().setActiveProfiles("s3");
+        // Start container
+        localStackContainer.start();
+
+        lcs = (LocalStackContainer) localStackContainer;
+        var accessKey = "aws.accessKeyId=" + lcs.getDefaultCredentialsProvider().getCredentials().getAWSAccessKeyId();
+        var secretKey = "aws.secretKey=" + lcs.getDefaultCredentialsProvider().getCredentials().getAWSSecretKey();
+        var endpoint = "aws.endpoint=" + lcs.getEndpointConfiguration(S3).getServiceEndpoint();
+        var region = "aws.region=" + lcs.getEndpointConfiguration(S3).getSigningRegion();
+
+        LOGGER.info("Starting LocalStack S3 with accessKey: {}, secretKey: {}, endpoint: {}, and region: {}", accessKey, secretKey, endpoint, region);
+        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(context, accessKey, secretKey, endpoint, region);
     }
 }
