1abb82f83bce2650573e5edcf84d39aa9411fefc
m78233 <61839550+m78233@users.noreply.github.com>
2021-05-12 16:31:41 +0100

    PAL-1022-s3-data-reader: fixes related to merge for S3DataReaderTest

--- s3-reader/src/test/java/uk/gov/gchq/palisade/service/data/s3/S3DataReaderTest.java
+++ s3-reader/src/test/java/uk/gov/gchq/palisade/service/data/s3/S3DataReaderTest.java
@@ -34,6 +34,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 
 import uk.gov.gchq.palisade.resource.LeafResource;
